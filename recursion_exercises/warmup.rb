7156b409877739cb24cbd5f77fe4c6aead3ee0d6
Manuel P <m.perezasensio@gmail.com>
2023-06-07 14:06:25 -0700

    debugging bsearch

--- recursion_exercises/warmup.rb
+++ recursion_exercises/warmup.rb
@@ -36,6 +36,22 @@ def fibonacci(n)
 end
 
 def bsearch(array, target)
+    return 'not found' if array.length == 0
+    idx = array.length / 2
+    return idx if array[idx] == target
+    if target < array[idx]
+        bsearch(array[0...idx], target)
+    else
+        bsearch(array[idx + 1..-1], target)
+    end
+    
     
 end
 
+p bsearch([1, 2, 3], 1) # => 0
+p bsearch([2, 3, 4, 5], 3) # => 1
+p bsearch([2, 4, 6, 8, 10], 6) # => 2
+p bsearch([1, 3, 4, 5, 9], 5) # => 3
+p bsearch([1, 2, 3, 4, 5, 6], 6) # => 5
+p bsearch([1, 2, 3, 4, 5, 6], 0) # => nil
+p bsearch([1, 2, 3, 4, 5, 7], 6) # => nil
